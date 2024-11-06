e46d39f338891f53d4a3b3ae48a327ef42eeb725
Megan Gialluca <gialluca@uw.edu>
2024-11-06 07:38:33 -0800

    typo

--- Run_Pipeline.py
+++ Run_Pipeline.py
@@ -2,7 +2,7 @@
 from Pipeline import *
 
 test_object = VPLModelingPipeline('TestPipelineAutoRun_EarthlikeH2O_T1c', 
-                                  '/gscratch/vsm/gialluca/VPLModelingTools_Dev/VPLModelingSupportScripts/Bodies/T1c/TestPipeline_T1cH2OOutgas_Initial', 
+                                  '/gscratch/vsm/gialluca/VPLModelingTools_Dev/VPLModelingSupportScripts/Bodies/T1c/TestPipeline_T1cH2OOutgas_Initial/', 
                                   True, find_molecules_of_interest=False, hitran_year='2020')
 
 test_object.run_automatic()
