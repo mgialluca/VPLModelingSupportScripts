9ee2e23d2536b0d59dc4984f51c0519287db62d0
Megan Gialluca <gialluca@uw.edu>
2025-01-22 13:01:18 -0800

    fix

--- Run_ParamSweep.py
+++ Run_ParamSweep.py
@@ -2,7 +2,7 @@ from Parameter_Sweep import *
 
 test_object = Generate_Atmosphere_Parameter_Sweep('TestSweep', 
                                   '/gscratch/vsm/gialluca/VPLModelingTools_Dev/VPLModelingSupportScripts/Bodies/T1c/T1cOutgas_Testing/', 
-                                  find_molecules_of_interest=False, hitran_year='2020')
+                                hitran_year='2020')
 
 
 test_object.run_grid_sweep()
