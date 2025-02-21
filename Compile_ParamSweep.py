a39c48b4e1ff2abfebbe57d3a6034b170ce8306b
Megan Gialluca <gialluca@uw.edu>
2025-02-21 10:08:24 -0800

    compiling latest run

--- Compile_ParamSweep.py
+++ Compile_ParamSweep.py
@@ -2,7 +2,7 @@ from Parameter_Sweep import *
 import subprocess
 
 
-test_object = Generate_Atmosphere_Parameter_Sweep('SweepTry5', 
+test_object = Generate_Atmosphere_Parameter_Sweep('SweepTry6', 
                                   '/gscratch/vsm/gialluca/VPLModelingTools_Dev/VPLModelingSupportScripts/Bodies/StartStateSweep1/', 
                                 restart_run= False, 
                                 hitran_year='2020')
