74e178f12a932830a974f6a377e45017d115b45a
Megan Gialluca <gialluca@uw.edu>
2025-02-24 11:18:56 -0800

    compiling restart input options

--- Compile_ParamSweep.py
+++ Compile_ParamSweep.py
@@ -2,12 +2,12 @@ from Parameter_Sweep import *
 import subprocess
 
 
-test_object = Generate_Atmosphere_Parameter_Sweep('SweepTry6', 
+test_object = Generate_Atmosphere_Parameter_Sweep('SweepTry7', 
                                   '/gscratch/vsm/gialluca/VPLModelingTools_Dev/VPLModelingSupportScripts/Bodies/StartStateSweep1/', 
                                 restart_run= False, 
                                 hitran_year='2020')
 
-test_object.compile_info_failed_run()
-#test_object.compile_restart_input_options()
+#test_object.compile_info_failed_run()
+test_object.compile_restart_input_options()
 #test_object.run_grid_sweep()
 #test_object.compile_run_output()
