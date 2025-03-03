e94aa8bd6590c3883151c5309d67db9899ea0c10
Megan Gialluca <gialluca@uw.edu>
2025-03-03 15:17:28 -0800

    prepping next sweep

--- Change_inputs.py
+++ Change_inputs.py
@@ -1,65 +1,59 @@
 import subprocess
 import os
 
-for i in range(1, 9):
-
-    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
-    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber0/PhotochemInputs/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
-    
-
-for i in [11, 12]:
-
-    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
-    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber10/PhotochemInputs/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
-    
-
-for i in [13, 16]:
-
-    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
-    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber14/PhotochemInputs/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
-    
-
 for i in range(32, 50):
 
-    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
-    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber31/PhotochemInputs/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
-   
-for i in range(50, 59):
-
-    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
-    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber0/PhotochemInputs/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
+    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
+    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/VPLModelingSupportScripts/InputDir_1bar/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
     
-
-for i in range(59, 63):
-
-    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
-    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber63/PhotochemInputs/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
-    
-
 for i in range(82, 100):
 
-    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
-    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber81/PhotochemInputs/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
-   
-for i in range(100, 106):
-
-    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
-    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber106/PhotochemInputs/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
-    
-
-for i in range(107, 109):
-
-    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
-    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber106/PhotochemInputs/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
-    
-
-for i in range(111, 114):
-
-    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
-    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber110/PhotochemInputs/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
+    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
+    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/VPLModelingSupportScripts/InputDir_1bar/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
     
 for i in range(132, 150):
 
-    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
-    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/VPLModelingSupportScripts/InputDir_ForTesting/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T2/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
+    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
+    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/VPLModelingSupportScripts/InputDir_1bar/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
+    
+
+for i in range(100, 105):
+
+    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
+    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber105/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
+   
+for i in range(107, 110):
+
+    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
+    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber106/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
+   
+for i in range(111, 113):
+
+    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
+    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber110/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
+   
+for i in [50, 52, 54, 55, 56, 57]:
+
+    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
+    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber51/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
+   
+for i in range(59, 63):
+
+    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
+    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber58/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
+   
+for i in [0, 1, 2, 3, 5, 6, 7]:
+
+    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
+    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber4/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
+   
+for i in [9]:
+
+    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
+    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber8/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
+   
+for i in [12, 14, 16]:
+
+    subprocess.run('rm /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/*', shell=True)
+    subprocess.run('cp -r /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber13/* /gscratch/vsm/gialluca/VPLModelingTools_Dev/Vdep4e-1T3/RunNumber'+str(i)+'/PhotochemInputs/', shell=True)
    
