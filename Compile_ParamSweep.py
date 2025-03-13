cbc5bc030b36617eb767602df4605330dca01110
Megan Gialluca <gialluca@n3240.hyak.local>
2025-03-12 17:11:40 -0700

    syncing

--- Compile_ParamSweep.py
+++ Compile_ParamSweep.py
@@ -2,14 +2,14 @@ from Parameter_Sweep import *
 import subprocess
 
 
-test_object = Generate_Atmosphere_Parameter_Sweep('Vdep4e-1T3', 
+test_object = Generate_Atmosphere_Parameter_Sweep('Vdep4e-1HighEsc', 
                                   '/gscratch/vsm/gialluca/VPLModelingTools_Dev/SmallerRange/RunNumber53/PhotochemInputs/', 
                                 restart_run= False, 
                                 hitran_year='2020')
 
-#test_object.compile_info_failed_run(Num_of_Models=150)
+test_object.compile_info_failed_run(Num_of_Models=150)
 
-test_object.compile_smart_spectra(Num_of_Models=150)
+#test_object.compile_smart_spectra(Num_of_Models=150)
 
 #test_object.compile_restart_input_options()
 #test_object.run_grid_sweep()
