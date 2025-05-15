9e03c971552511893bc41f1a7e2dd4a379e095ad
Megan Gialluca <gialluca@uw.edu>
2025-05-15 11:48:01 -0700

    prepping multinest test

--- Run_MCMC.py
+++ Run_MCMC.py
@@ -2,10 +2,10 @@ from Parameter_Sweep import *
 import subprocess
 
 
-test_object = Generate_Atmosphere_Parameter_Sweep('MCMCTest', 
+test_object = Generate_Atmosphere_Parameter_Sweep('MultiNTest', 
                                   '/gscratch/vsm/gialluca/VPLModelingTools_Dev/VeffTestDepos/RunNumber6/PhotochemInputs/', 
-                                restart_run= True, 
-                                starting_point=None,
+                                restart_run= 'VeffTestDepos', 
+                                starting_point='Euclidean',
                                 hitran_year='2020')
 
-test_object.match_surf_pressure_MCMC()
+test_object.match_data_multinest()
