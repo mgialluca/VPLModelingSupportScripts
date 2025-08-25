432ef1cf9140e39859e4f36d1bf8c837667a9a9e
Megan Gialluca <gialluca@uw.edu>
2025-08-25 10:38:16 -0700

    resetting B zoom 1

--- Run_ParamSweep.py
+++ Run_ParamSweep.py
@@ -15,21 +15,12 @@ test_object = Generate_Atmosphere_Parameter_Sweep('Bzoom1',
 # Outgassing rate for T1b
 test_object.outgass_species_MinMax_gridsweep['H2O'] = [3.60355158e10, 1.01148276e12]#[45737798.59485552, 1.01148276e12]
 
-'''
-test_object.outgass_sample_resolution_gridsweep = [4]
+test_object.outgass_sample_resolution_gridsweep = [16]
 
-test_object.escape_sample_type_gridsweep = ['Linear', 'UserDef', 'UserDef', 'UserDef']
-test_object.escape_sample_resolution_gridsweep = [5, 0, 0, 0]
-
-test_object.escape_samples_gridsweep['O'] = []
-test_object.escape_species_MinMax_gridsweep['O'] = [0.175, 0.835]
-
-test_object.escape_samples_gridsweep['O2'] = [0.1]#[1e26, 5e26, 1e27]
+test_object.escape_samples_gridsweep['O'] = [0.1, 0.5]
+test_object.escape_samples_gridsweep['O2'] = [0.05, 0.1]
 test_object.escape_samples_gridsweep['O3'] = [0.4] 
 test_object.escape_samples_gridsweep['H2O2'] = [1]
-
-#test_object.compile_info_failed_run()
-'''
 
 test_object.run_grid_sweep()
 test_object.compile_run_output()
