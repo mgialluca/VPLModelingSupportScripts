dc3142b6e3eaab471f8b03ccfe1d30f97f1d8475
Megan Gialluca <gialluca@uw.edu>
2025-08-25 11:30:59 -0700

    fixing radius mistake

--- Run_ParamSweep_T1f.py
+++ Run_ParamSweep_T1f.py
@@ -3,7 +3,7 @@ import subprocess
 
 
 #'/gscratch/vsm/gialluca/VPLModelingTools_Dev/SensTestH2O/RunNumber175/
-test_object = Generate_Atmosphere_Parameter_Sweep('Fhighn2', 
+test_object = Generate_Atmosphere_Parameter_Sweep('Finit', 
                                   '/gscratch/vsm/gialluca/VPLModelingTools_Dev/UpdatedStarts/T1fSt/PhotochemInputs/', 
                                 restart_run= True, 
                                 starting_point='Exact',
@@ -13,8 +13,15 @@ test_object = Generate_Atmosphere_Parameter_Sweep('Fhighn2',
                                 planet='T1f')
 
 # Outgassing rate for T1h
-test_object.outgass_species_MinMax_gridsweep['H2O'] = [44380169.91551189, 7.39523609e11]
-test_object.adjust_N2 = 0.05 # 0.05 bars of N2 fixed
+test_object.outgass_species_MinMax_gridsweep['H2O'] = [48906843.61425449, 8.1495329e11]
+#test_object.adjust_N2 = 0.05 # 0.05 bars of N2 fixed
+
+test_object.outgass_sample_resolution_gridsweep = [4]
+
+test_object.escape_samples_gridsweep['O'] = [0.01, 0.1]
+test_object.escape_samples_gridsweep['O2'] = [0.01, 0.05]
+test_object.escape_samples_gridsweep['O3'] = [0.02, 0.4] 
+test_object.escape_samples_gridsweep['H2O2'] = [0.02, 0.4]
 
 #test_object.compile_info_failed_run()
 test_object.run_grid_sweep()
