ff8f445fdf5889e02223d1fb40b3ba884069fb79
Megan Gialluca <gialluca@uw.edu>
2025-10-04 11:58:38 -0700

    w loss co2 sweeps outer planets

--- Run_ParamSweep_T1h.py
+++ Run_ParamSweep_T1h.py
@@ -3,7 +3,7 @@ import subprocess
 
 
 #'/gscratch/vsm/gialluca/VPLModelingTools_Dev/SensTestH2O/RunNumber175/
-test_object = Generate_Atmosphere_Parameter_Sweep('Hco2lo', 
+test_object = Generate_Atmosphere_Parameter_Sweep('Hco2wL', 
                                   '/gscratch/vsm/gialluca/VPLModelingTools_Dev/UpdatedStarts/T1hco2/', 
                                 restart_run= True, 
                                 starting_point='Exact',
@@ -22,10 +22,19 @@ test_object.outgass_sample_type_gridsweep = ['Log', 'Log']
 
 test_object.outgass_sample_resolution_gridsweep = [4, 4]
 
-test_object.escape_samples_gridsweep['O'] = [0.001, 0.01] #[0.00001, 0.000001]
-test_object.escape_samples_gridsweep['O2'] = [1e-5, 1e-6]#[0.00001, 0.000001]
+test_object.escape_species_gridsweep = ['O', 'O2', 'O3', 'H2O2', 'CO', 'CO2'] # Species to vary escape rates of
+test_object.escape_species_losstype = ['Veff', 'Veff', 'Vdep', 'Vdep', 'Vdep', 'Veff']
+test_object.escape_species_molarmass['CO'] = [28.01]*(u.g/u.mol) 
+test_object.escape_species_molarmass['CO2'] = [44.01]*(u.g/u.mol)
+test_object.escape_sample_type_gridsweep = ['UserDef', 'UserDef', 'UserDef', 'UserDef', 'UserDef', 'UserDef'] 
+test_object.escape_species_units_gridsweep = [u.cm/u.s, u.cm/u.s, u.cm/u.s, u.cm/u.s, u.cm/u.s, u.cm/u.s]
+
+test_object.escape_samples_gridsweep['O'] = [0.01, 0.1]
+test_object.escape_samples_gridsweep['O2'] = [0.01, 0.05]
 test_object.escape_samples_gridsweep['O3'] = [0.02] 
 test_object.escape_samples_gridsweep['H2O2'] = [0.02]
+test_object.escape_samples_gridsweep['CO'] = [0.0, 0.01]
+test_object.escape_samples_gridsweep['CO2'] = [0.01, 0.1]
 
 
 #test_object.compile_info_failed_run()
