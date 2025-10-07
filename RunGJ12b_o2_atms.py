e492e30b85835b006058530ce2689372333dcb24
Megan Gialluca <gialluca@uw.edu>
2025-10-07 16:17:45 -0700

    more gj12b atmospheres

--- RunGJ12b_o2_atms.py
+++ RunGJ12b_o2_atms.py
@@ -83,22 +83,22 @@ def run_starting_points(case):
     
     master = '/gscratch/vsm/gialluca/VPLModelingTools_Dev/'
 
-    if case == 'GbdryCO2b01':
-        initin = '/gscratch/vsm/gialluca/VPLModelingTools_Dev/GJ12b_Starts/CO2_01bar/'
+    if case == 'GbO2-CO2b01':
+        initin = '/gscratch/vsm/gialluca/VPLModelingTools_Dev/GJ12b_Starts/O2-CO2_01bar/'
         planet = 'GJ12b'
         gas_names = ['O2', 'H2O', 'O3', 'CO2', 'CO']
 
-    elif case == 'GbdryCO2b1':
-        initin = '/gscratch/vsm/gialluca/VPLModelingTools_Dev/GJ12b_Starts/CO2_1bar/'
+    elif case == 'GbO2-CO2b1':
+        initin = '/gscratch/vsm/gialluca/VPLModelingTools_Dev/GJ12b_Starts/O2-CO2_1bar/'
         planet = 'GJ12b'
         gas_names = ['O2', 'H2O', 'O3', 'CO2', 'CO']
 
-    elif case == 'Gb2xO2b01':
+    elif case == 'Gb10xO2b01':
         initin = '/gscratch/vsm/gialluca/VPLModelingTools_Dev/GJ12b_Starts/O2_01bar/'
         planet = 'GJ12b'
         gas_names = ['O2', 'H2O', 'O3', 'H2O2']
 
-    elif case == 'Gb2xO2b1':
+    elif case == 'Gb10xO2b1':
         initin = '/gscratch/vsm/gialluca/VPLModelingTools_Dev/GJ12b_Starts/O2_1bar/'
         planet = 'GJ12b'
         gas_names = ['O2', 'H2O', 'O3', 'H2O2']
@@ -127,7 +127,7 @@ def run_starting_points(case):
 
 
 
-inputs = ['Gb2xO2b01', 'Gb2xO2b1', 'GbdryCO2b01', 'GbdryCO2b1']#['b01', 'b1', 'b10']
+inputs = ['Gb10xO2b01', 'Gb10xO2b1', 'GbO2-CO2b01', 'GbO2-CO2b1']#['b01', 'b1', 'b10']
 
 with Pool() as p:
     models = p.map(run_starting_points, inputs)
