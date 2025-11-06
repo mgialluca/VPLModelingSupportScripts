419b025be26d985081e8d3d8e9635159a90f44cb
Megan Gialluca <gialluca@uw.edu>
2025-11-06 06:29:03 -0800

    setting up for T1e

--- Run_SO2_Additions_T1e.py
+++ Run_SO2_Additions_T1e.py
@@ -5,7 +5,7 @@ import os
 from multiprocessing import Pool
 import os
 
-# Currently set up for: TRAPPIST-1C
+# Currently set up for: TRAPPIST-1E
 
 
 def set_pipeline_vars(casename, pipelineobj, master_out, gas_names = ['O2', 'H2O', 'O3']):
@@ -36,7 +36,7 @@ def set_pipeline_vars(casename, pipelineobj, master_out, gas_names = ['O2', 'H2O
 
     # Adjust the atmospheric pressure
     pipelineobj.adjust_atmospheric_pressure = False
-    pipelineobj.include_2column_climate = True # Might need to change
+    pipelineobj.include_2column_climate = False # Might need to change
     pipelineobj.suppress_IOerrors = True
     pipelineobj.run_spectra = True
     pipelineobj.dayside_starting_PT = None
@@ -47,7 +47,7 @@ def set_pipeline_vars(casename, pipelineobj, master_out, gas_names = ['O2', 'H2O
 
     pipelineobj.MCMC_pressure_only = False
     pipelineobj.MultiNest_DataFit = False
-    pipelineobj.rerun_smart_for_2col = True # Might need to change
+    pipelineobj.rerun_smart_for_2col = False # Might need to change
     
     if pipelineobj.MCMC_pressure_only == True:
         pipelineobj.include_2column_climate = False
@@ -154,7 +154,7 @@ def run_one_model(inputs):
 
     pipelineobj = VPLModelingPipeline(case, 
                                     initin, 
-                                    True, find_molecules_of_interest=False, hitran_year='2020', planet='T1c')
+                                    True, find_molecules_of_interest=False, hitran_year='2020', planet='T1e')
     
     if atmtype == 'H2O-O2':
         gases = ['O2', 'H2O', 'O3', 'H2', 'SO2', 'SO'] # DOUBLE CHECK 
@@ -281,7 +281,7 @@ def populate_tracking_json(planet): # Want a function to run once that checks th
 
 # First need the tracking document
 
-planet = 'T1c' # CHANGES PLANET TO PLANET 
+planet = 'T1e' # CHANGES PLANET TO PLANET 
 avail_cores = 192 # in case we use a 40 core node
 
 if not os.path.exists('/gscratch/vsm/gialluca/VPLModelingTools_Dev/AddSO2/'+planet+'_Tracking.json'):
