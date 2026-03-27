7b9ed6d4990e57369d5f7a24db035b7a8f0c2a19
Megan Gialluca <gialluca@uw.edu>
2026-03-27 15:17:53 -0700

    isotherm modern earth at effective temp

--- Run_Earth_Cases.py
+++ Run_Earth_Cases.py
@@ -20,7 +20,7 @@ def Change_Tindist_to_isothermal(pipelineobj):
     # Find number of blocks of mixing ratios until T/EDD columns
     NQblocks = np.ceil(NQ/10)
 
-    new_T = np.ones(200)*288.4
+    new_T = np.ones(200)*255
 
     ### Now to create new in.dist
 
@@ -74,7 +74,7 @@ def Change_Tindist_to_isothermal(pipelineobj):
 
 
 
-pipelineobj = VPLModelingPipeline('IsoEarth2', 
+pipelineobj = VPLModelingPipeline('IsoTeff', 
                                   '/gscratch/vsm/gialluca/VPLModelingTools_Dev/atmos/PHOTOCHEM/INPUTFILES/TEMPLATES/ModernEarth/', 
                                   True, find_molecules_of_interest=False, hitran_year='2020', planet='Earth')
 
