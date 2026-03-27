57d67c550e444ef7d539cd4f9e2ee10b36913e64
Megan Gialluca <gialluca@uw.edu>
2026-03-27 10:00:10 -0700

    unfixing co2 for modern earth isotherm

--- Run_Earth_Cases.py
+++ Run_Earth_Cases.py
@@ -20,7 +20,7 @@ def Change_Tindist_to_isothermal(pipelineobj):
     # Find number of blocks of mixing ratios until T/EDD columns
     NQblocks = np.ceil(NQ/10)
 
-    new_T = np.ones(200)*278.6
+    new_T = np.ones(200)*288.4
 
     ### Now to create new in.dist
 
@@ -74,8 +74,8 @@ def Change_Tindist_to_isothermal(pipelineobj):
 
 
 
-pipelineobj = VPLModelingPipeline('IsoArch2', 
-                                  '/gscratch/vsm/gialluca/VPLModelingTools_Dev/atmos/PHOTOCHEM/INPUTFILES/TEMPLATES/Archean+haze/', 
+pipelineobj = VPLModelingPipeline('IsoEarth2', 
+                                  '/gscratch/vsm/gialluca/VPLModelingTools_Dev/atmos/PHOTOCHEM/INPUTFILES/TEMPLATES/ModernEarth/', 
                                   True, find_molecules_of_interest=False, hitran_year='2020', planet='Earth')
 
 master_out = '/gscratch/vsm/gialluca/PostDocPropose/Earth_Isotherm_Test/'
