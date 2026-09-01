339228b0ec690798348a4b6eb0d7f1f561dc82d3
Megan Gialluca <mgialluca@localhost.localdomain>
2026-09-01 14:54:29 -0700

    prepping GJ12 co2 runs

--- Run_ParamSweep_GJ12b_Brady.py
+++ Run_ParamSweep_GJ12b_Brady.py
@@ -3,7 +3,7 @@ import subprocess
 
 
 #'/gscratch/vsm/gialluca/VPLModelingTools_Dev/SensTestH2O/RunNumber175/
-test_object = Generate_Atmosphere_Parameter_Sweep('GJ12bH2O', 
+test_object = Generate_Atmosphere_Parameter_Sweep('GJ12bCO2', 
                                   '/gscratch/vsm/gialluca/VPLModelingTools_Dev/GJ12b_Starts/O2Dom_Updated_Brady/', 
                                 restart_run= False, 
                                 starting_point='Exact',
@@ -12,24 +12,28 @@ test_object = Generate_Atmosphere_Parameter_Sweep('GJ12bH2O',
                                 spectra=True,
                                 planet='GJ12b_Brady')
 
-# Outgassing rate for T1c
-test_object.outgass_species_gridsweep = ['H2O']
-test_object.outgass_species_MinMax_gridsweep['H2O'] = [34208000000.0, 9.47899801e11]#[34208000000.0, 9.47899801e11]
-test_object.outgass_sample_type_gridsweep = ['Log']
-test_object.outgass_species_sourcetype = ['Flx']
+test_object.outgass_species_gridsweep = ['H2O', 'CO2']
+test_object.outgass_species_MinMax_gridsweep['H2O'] = [0, 4.8779e11]#[34208000000.0, 9.47899801e11]
+test_object.outgass_species_MinMax_gridsweep['CO2'] = [88539.0, 5.5e10]#[34208000000.0, 9.47899801e11]
+test_object.outgass_sample_type_gridsweep = ['Log', 'Log']
+test_object.outgass_species_sourcetype = ['Flx', 'Flx']
+test_object.outgass_species_molarmass['CO2'] = [44.01]*(u.g/u.mol)
 
-test_object.outgass_sample_resolution_gridsweep = [6]
+test_object.outgass_sample_resolution_gridsweep = [3, 3]
 
-test_object.escape_species_gridsweep = ['O', 'O2', 'O3', 'H2O2', 'CO'] # Species to vary escape rates of
-test_object.escape_species_losstype = ['Veff', 'Veff', 'Vdep', 'Vdep', 'Vdep']
-test_object.escape_sample_type_gridsweep = ['UserDef', 'UserDef', 'UserDef', 'UserDef', 'UserDef'] 
-test_object.escape_species_units_gridsweep = [u.cm/u.s, u.cm/u.s, u.cm/u.s, u.cm/u.s, u.cm/u.s]
+test_object.escape_species_gridsweep = ['O', 'O2', 'O3', 'H2O2', 'CO', 'CO2'] # Species to vary escape rates of
+test_object.escape_species_losstype = ['Veff', 'Veff', 'Vdep', 'Vdep', 'Vdep', 'Veff']
+test_object.escape_sample_type_gridsweep = ['UserDef', 'UserDef', 'UserDef', 'UserDef', 'UserDef', 'UserDef'] 
+test_object.escape_species_units_gridsweep = [u.cm/u.s, u.cm/u.s, u.cm/u.s, u.cm/u.s, u.cm/u.s, u.cm/u.s]
+test_object.escape_species_molarmass['CO'] = [28.01]*(u.g/u.mol) 
+test_object.escape_species_molarmass['CO2'] = [44.01]*(u.g/u.mol)
 
-test_object.escape_samples_gridsweep['O'] = [0.001, 0.01, 0.1]
-test_object.escape_samples_gridsweep['O2'] = [0.001, 0.01, 0.1]
+test_object.escape_samples_gridsweep['O'] = [0.001, 0.01]
+test_object.escape_samples_gridsweep['O2'] = [0.001, 0.01]
 test_object.escape_samples_gridsweep['O3'] = [0.02] 
 test_object.escape_samples_gridsweep['H2O2'] = [0.02]
 test_object.escape_samples_gridsweep['CO'] = [0.3]
+test_object.escape_samples_gridsweep['CO2'] = [0.001, 0.01]
 
 test_object.run_grid_sweep()
 test_object.compile_run_output()
